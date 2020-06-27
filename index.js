5abfb8f8c327013be23e389f5841fa195e2586b7
AnaJL <analinsdev@gmail.com>
2020-06-27 20:00:09 -0300

    praticando js - chall 5

---
+++ index.js
@@ -0,0 +1,16 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks, show;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tablinks = document.getElementsByClassName("tablinks");
+       for (i = 0; i < tabcontent.length; i++) { //esconde todos os conteúdos
+       tabcontent[i].style.display = "none";
+       }
+       for (i = 0; i < tablinks.length; i++) { //tira o active de todos button
+       tablinks[i].className = tablinks[i].className.replace(" active", "");
+       }
+       if (cityName != 'Clean'){
+           document.getElementById(cityName).style.display = "inline-flex"; //deixa o conteúdo visível
+           
+           evt.currentTarget.className += " active"; //deixa o button marcado
+   }}
+
