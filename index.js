f7f534ecfb8ec0f8272f9b0407dc3c4239f13405
AnaJL <analinsdev@gmail.com>
2020-06-27 20:26:49 -0300

    melhorando CSS

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks, show;
+function openCity(event, cityName) {
+    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     tablinks = document.getElementsByClassName("tablinks");
+    
        for (i = 0; i < tabcontent.length; i++) { //esconde todos os conteúdos
        tabcontent[i].style.display = "none";
        }
+
        for (i = 0; i < tablinks.length; i++) { //tira o active de todos button
        tablinks[i].className = tablinks[i].className.replace(" active", "");
        }
+
        if (cityName != 'Clean'){
            document.getElementById(cityName).style.display = "inline-flex"; //deixa o conteúdo visível
-           
-           evt.currentTarget.className += " active"; //deixa o button marcado
+           event.currentTarget.className += " active"; //deixa o button marcado
    }}
 
